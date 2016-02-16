96857ea700cf39d2f0c0ff7b4589fc39d48e76ca
Avi <avinash.jairam@gmail.com>
2016-02-15 23:47:50 -0500

    Working on resetting stored user input each time a new category is selected

--- word.js
+++ word.js
@@ -3,30 +3,3 @@
 //-Detecting which category is selected
 //-Pick a random word from the category selected
 
-var word = function(){
-		var numWords;
-		var wordIndex;
-		var category;
-		
-		this.getSelectedCategory = function(){
-			var e = document.getElementById("menu");
-			this.category=e.options[e.selectedIndex].value;
-		}
-
-		this.setNumCategories = function(num){
-			this.numCategories = num;
-		}
-
-		this.setWordIndex = function(){
-			this.wordIndex = Math.floor((Math.random() * this.numCategories) );
-		}
-
-		this.getNumWords =function(){
-
-		}
-
-		//This function will read the contents of the file 
-		this.readFile = function(fileName){
-
-		}
-}
